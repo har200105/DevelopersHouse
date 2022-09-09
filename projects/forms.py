202bab1401a8cbd922b09b409c2be933ad660c26
Harshit Rathi <harshitr2001@gmail.com>
2022-09-09 12:02:42 +0530

    changes

--- projects/forms.py
+++ projects/forms.py
@@ -19,12 +19,6 @@ class ProjectForm(ModelForm):
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
 
-        # self.fields['title'].widget.attrs.update(
-        #     {'class': 'input'})
-
-        # self.fields['description'].widget.attrs.update(
-        #     {'class': 'input'})
-
 
 class ReviewForm(ModelForm):
     class Meta:
